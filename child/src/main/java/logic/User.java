f4152979974f9068fdd119fa483b9f29c5df655a
gd5-16 <gd5-16@DESKTOP-S6HH7HB>
2018-12-31 17:03:34 +0900

    logic user로 수정

---
+++ child/src/main/java/logic/User.java
@@ -0,0 +1,41 @@
+package logic;
+
+public class User {
+	private int mnum;
+	private String email;
+	private String nickname;
+	private String password;
+
+	public int getMnum() {
+		return mnum;
+	}
+
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
