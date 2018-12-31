e59acbaa85566eafbd6caa797ceae688c5f06476
gd5-16 <gd5-16@DESKTOP-S6HH7HB>
2018-12-31 16:40:55 +0900

    logic 다 했습니당~

--- child/src/main/java/logic/Board.java
+++ child/src/main/java/logic/Board.java
@@ -1,9 +1,114 @@
 package logic;
 
+import java.util.Date;
+
 public class Board {
 	private int bnum;
 	private int mnum;
 	private int btype;
 	private int head;
 	private String subject;
+	private String content;
+	private Date regdate;
+	private int readcnt;
+	private String file1;
+	private String file2;
+	private String file3;
+	private int red;
+
+	public int getBnum() {
+		return bnum;
+	}
+
+	public void setBnum(int bnum) {
+		this.bnum = bnum;
+	}
+
+	public int getMnum() {
+		return mnum;
+	}
+
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+
+	public int getBtype() {
+		return btype;
+	}
+
+	public void setBtype(int btype) {
+		this.btype = btype;
+	}
+
+	public int getHead() {
+		return head;
+	}
+
+	public void setHead(int head) {
+		this.head = head;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getReadcnt() {
+		return readcnt;
+	}
+
+	public void setReadcnt(int readcnt) {
+		this.readcnt = readcnt;
+	}
+
+	public String getFile1() {
+		return file1;
+	}
+
+	public void setFile1(String file1) {
+		this.file1 = file1;
+	}
+
+	public String getFile2() {
+		return file2;
+	}
+
+	public void setFile2(String file2) {
+		this.file2 = file2;
+	}
+
+	public String getFile3() {
+		return file3;
+	}
+
+	public void setFile3(String file3) {
+		this.file3 = file3;
+	}
+
+	public int getRed() {
+		return red;
+	}
+
+	public void setRed(int red) {
+		this.red = red;
+	}
 }
