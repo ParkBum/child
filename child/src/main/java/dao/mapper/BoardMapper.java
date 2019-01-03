4f0529aa3640af83fa5b5e19058905c12f609499
jobori <joara0625@gmail.com>
2019-01-03 19:05:51 +0900

    insert 쿼리문 추가

--- child/src/main/java/dao/mapper/BoardMapper.java
+++ child/src/main/java/dao/mapper/BoardMapper.java
@@ -1,5 +1,6 @@
 package dao.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import logic.Board;
@@ -8,5 +9,9 @@ public interface BoardMapper {
 
 	@Select("select * from board where bnum = #{bnum}")
 	Board select(Integer bnum);
+
+	@Insert("insert into board (bnum,mnum,btype,head,subject,content,regdate,readcnt,file1,file2,file3,red,score) "
+			+ "values (#{bnum},#{mnum},#{btype},#{head},#{subject},#{content},now(),#{readcnt},#{file1},#{file2},#{file3},0,#{score})")
+	void insert(Board board);
 
 }
