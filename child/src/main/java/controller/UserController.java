95cb74c6b73f3cc1f218155bb798007119be6009
ParkBum <33215013+ParkBum@users.noreply.github.com>
2019-01-03 15:05:51 +0900

    mail수정중

--- child/src/main/java/controller/UserController.java
+++ child/src/main/java/controller/UserController.java
@@ -1,8 +1,21 @@
 package controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -11,6 +24,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import logic.ChildService;
@@ -69,5 +83,64 @@ public class UserController {
 			br.reject("error.duplicate.user");
 		}
 		return mav;
+	}
+	
+	@RequestMapping("user/emailAuth")
+	public ModelAndView entry(User user) {
+		ModelAndView mav = new ModelAndView();
+		String email = user.getEmail();
+		String authNum = "";
+		authNum = RandomNum();
+		sendEmail(email.toString(), authNum);
+		mav.setViewName("user/emailAuth");
+		mav.addObject("email",email);
+		mav.addObject("authNum",authNum);
+		return mav;
+	}
+
+	private void sendEmail(String email, String authNum) {
+		String from = "20dy@naver.com";
+		try{
+		 Properties prop = new Properties();	
+		 prop.put("mail.smtp.host"	,"smtp.naver.com");	
+		 prop.put("mail.smtp.starttls.enable"	,"true");
+//		 prop.put("mail.user", mail.getNaverid());
+//		 prop.put("mail.from", mail.getNaverid());
+		 prop.put("mail.debug", "true");				
+		 prop.put("mail.smtp.auth", "true");			 
+		 prop.put("mail.smtp.port", "465");
+		 prop.put("mail.smtp.user", email);
+		 prop.put("mail.smtp.socketFactory.port", "465");
+		 prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		 prop.put("mail.smtp.socketFactory.fallback", "false");
+		 Session session = Session.getInstance(prop,
+				 new javax.mail.Authenticator() {
+			 protected PasswordAuthentication getPassswordAuthentication() {
+				 return new PasswordAuthentication("20dy@naver.com", "park0405!@!");
+			 }
+		 });	//메일서버에 연결
+		 String content = "인증번호는 ["+authNum+"] 입니다.";
+		 MimeMessage msg = new MimeMessage(session);	//메일로 전송할 객체 생성 
+			msg.setFrom(new InternetAddress(from,MimeUtility.encodeText("운영자","UTF-8","B")));
+			InternetAddress[] addr = {new InternetAddress(email)};
+			msg.setRecipients(Message.RecipientType.TO, addr);	// 보낸 사람, 여러명 
+			msg.setSubject("인증번호 발급");
+			msg.setSentDate(new Date());	//보낸 날짜
+			msg.setContent(content,"text/html;charset=euc-kr");
+			Transport.send(msg);	//메일 전송 실행
+		} catch (MessagingException me) {
+			me.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String RandomNum() {
+		StringBuffer br = new StringBuffer();
+		for(int i=0; i<=6; i++) {
+			int n = (int)(Math.random() * 10);
+			br.append(n);
+		}
+		return br.toString();
 	}
 }
--- child/src/main/webapp/WEB-INF/view/user/userForm.jsp
+++ child/src/main/webapp/WEB-INF/view/user/userForm.jsp
@@ -6,6 +6,9 @@
 <head>
 <meta charset="EUC-KR">
 <title>사용자 등록</title>
+<script>
+
+</script>
 <style type="text/css">
 .joinForm {
 	border-radius: 5px;
@@ -57,53 +60,24 @@ input[type=submit] :hover, input[type=reset]:hover {
 					</c:forEach>
 				</font>
 			</spring:hasBindErrors>
-			<%-- 	<table>
-		<tr height="40px">
-<!-- 			<td>이메일</td> -->
-			<td><form:input path="email" placeholder="아이디를 입력하세요(e-mail 형식)"/>
-			<font color="red"><form:errors path="email"/></font></td>
-		</tr>
-		<tr height="40px">
-			<td>인증번호</td>
-			<td><form:input path="email"/>
-			<font color="red"><form:errors path="email"/></font></td>
-		</tr>
-		<tr height="40px">
-			<td>비밀번호</td>
-			<td><form:password path="password"/>
-			<font color="red"><form:errors path="password"/></font></td>
-		</tr>
-		<tr height="40px">
-<!-- 			<td>비밀번호</td> -->
-			<td><form:password path="password" placeholder="비밀번호를 입력하세요"/>
-			<font color="red"><form:errors path="password"/></font></td>
-		</tr>
-		<tr height="40px">
-<!-- 			<td>닉네임</td> -->
-			<td><form:input path="nickname" placeholder="닉네임을 입력하세요"/>
-			<font color="red"><form:errors path="nickname"/></font></td>
-		</tr>
-		<tr height="40px">
-		<td colspan="2" align="center">
-		<input type="submit" value="가입">
-		<input type="reset" value="재작성">
-		</td></tr></table> --%>
+			
 			<div class="inin">
-				<form:input path="email" placeholder="아이디를 입력하세요(e-mail 형식)" />
+				<form:input path="email" placeholder="아이디를 입력하세요(e-mail 형식)" id="email" />
 				<font color="red"><form:errors path="email" /></font>
 			</div>
 			<div class="inin">
 			<button style="background-color:#33CC99; width:90%; border-radius: 10px;" onclick="javascript:alert('구현안됨')">인증번호 전송</button>
 			</div>
 			<div class="inin" style="display:inline;">
-				<div style="width:60%;">
-				<form:input path="email" placeholder="인증번호를 입력하세요"/>
-				<font color="red"><form:errors path="email" /></font>
+				<div style="width:60%;"><form:hidden path="authNum"/>
+				<form:input path="userauthNum" placeholder="인증번호를 입력하세요"/>
+				<font color="red"><form:errors path="userauthNum" /></font>
 				</div>
 				
-			<button style="background-color:#33CC99; width:30%; float:right; border-radius: 10px;" onclick="javascript:alert('구현안됨')">인증번호 전송</button>
-				
+			<button style="background-color:#33CC99; width:30%; 
+			float:right; border-radius: 10px;" onclick="javascript:check()">인증번호 확인</button>
 			</div>
+			
 			<div class="inin">
 				<form:password path="password" placeholder="비밀번호를 입력하세요" />
 				<font color="red"><form:errors path="password" /></font>
