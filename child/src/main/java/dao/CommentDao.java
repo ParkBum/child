3fbe5cded3a6fdf7d06854551888b4cc39c2022b
thfastang401 <thfastang401@naver.com>
2019-01-07 18:48:13 +0900

    댓글Dao

---
+++ child/src/main/java/dao/CommentDao.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import dao.mapper.CommentMapper;
+import logic.Comment;
+
+@Repository
+public class CommentDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	private final String NS = "dao.mapper.CommentMapper.";
+
+	public void commentWrite(Comment comment) {
+		sqlSession.getMapper(CommentMapper.class).insert(comment);
+	}
+
+	public List<Comment> commentSelect(Integer bnum) {
+		return sqlSession.getMapper(CommentMapper.class).selectComment(bnum);
+	}
+	
+}
