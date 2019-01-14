1e368ad46cb592af86791d210259c85dfe1cb677
jobori <joara0625@gmail.com>
2019-01-14 18:33:42 +0900

    보드 수정됩니다ㅠㅠ

--- child/src/main/java/logic/ChildService.java
+++ child/src/main/java/logic/ChildService.java
@@ -90,6 +90,8 @@ public class ChildService {
 			uploadFileCreate(board.getMulti3(), request, "file"); // file3의 내용을 파일로 저장
 			board.setFile3(board.getMulti3().getOriginalFilename()); // db에 파일명을 저장
 		}
+		Daycare dc = mapDao.selectOne(board.getCode());
+		board.setDcname(dc.getName());
 		boardDao.update(board);
 	}
 
