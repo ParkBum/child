9c9df7564fc75f525343b1f517a0e6e484f1761a
gdj-4 <gdj-4@DESKTOP-UFUK33O>
2019-01-21 17:14:11 +0900

    AOP, map.jsp 수정

--- child/src/main/java/aop/LoginAspect.java
+++ child/src/main/java/aop/LoginAspect.java
@@ -14,18 +14,14 @@ import logic.User;
 @Component
 @Aspect
 public class LoginAspect {
-	@Around("execution(* controller.User*.*(..)) && args(mnum,session,..)")
+	@Around("execution(* controller.User*.list(..)) && args(mnum,session,..)")
 	public Object userLoginCheck(ProceedingJoinPoint joinPoint,Integer mnum, HttpSession session) throws Throwable {
 		User loginUser = (User) session.getAttribute("loginUser");
 		//1. 로그인이 안된 경우
 		if(loginUser == null) {
 			throw new LoginException("로그인 후 이용하세요","../user/loginForm.child");
 		}
-		//2. admin이 아니면서 id와 로그인 정보가 다른 경우
-		if(!mnum.equals(loginUser.getMnum()) && !loginUser.getEmail().equals("admin@aaa.bbb")) {
-			throw new LoginException("본인만 가능합니다","../user/list.child?mnum="+loginUser.getMnum());
-		}
-	    
+	
 		Object ret = joinPoint.proceed();
 		return ret;
 	}
